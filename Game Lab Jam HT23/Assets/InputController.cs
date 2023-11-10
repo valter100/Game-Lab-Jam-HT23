2bef9edcaa0863f6ad15548967baf3483c8b48c1
valter100 <Va6648li-s@student.lu.se>
2023-11-10 23:36:27 +0100

    Movement + Camera

---
+++ Game Lab Jam HT23/Assets/InputController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+    [SerializeField] PlayerInput input;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMovement()
+    {
+
+    }
+}
