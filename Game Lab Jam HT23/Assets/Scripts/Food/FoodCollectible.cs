52240cd189a12d1122e8cdd13787897763c490c5
mixy02 <65550211+mixy02@users.noreply.github.com>
2023-11-10 21:40:16 +0100

    Food collectibles

---
+++ Game Lab Jam HT23/Assets/Scripts/Food/FoodCollectible.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodCollectible : MonoBehaviour
+{
+    [SerializeField] float rotateSpeed = 0.04f;
+    private float time;
+    [SerializeField] float bounceMultiplier = 0.0001f;
+    void Update()
+    {
+        time += Time.deltaTime;
+        Rotate();
+        Bounce();
+    }
+    private void Rotate()
+    {
+        transform.Rotate(new Vector3(0, rotateSpeed, 0));
+    }
+    private void Bounce()
+    {
+        float newY = Mathf.Sin(time)  * bounceMultiplier;
+        transform.position = new Vector3(transform.position.x, transform.position.y + newY, transform.position.z);
+    }
+    public void CollectFood()
+    {
+        Destroy(gameObject);
+    }
+}
