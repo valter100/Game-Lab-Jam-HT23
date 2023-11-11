95abb9e713b2eff10a013490968a77e23f43591a
mixy02 <65550211+mixy02@users.noreply.github.com>
2023-11-11 11:57:25 +0100

    Food spawning randomly

--- Game Lab Jam HT23/Assets/Scripts/Food/FoodManager.cs
+++ Game Lab Jam HT23/Assets/Scripts/Food/FoodManager.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FoodManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject[] foodPrefabs;
+    [SerializeField] GameObject[] foodSpawnPoints;
+
+    private void Awake()
     {
-        
+        GetSpawnPoints();
+        SpawnFood(0.2f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GetSpawnPoints()
     {
-        
+        foodSpawnPoints = GameObject.FindGameObjectsWithTag("FoodSpawn");
+        Array.Reverse(foodSpawnPoints);
+    }
+
+    public void SpawnFood(float difficulty)
+    {
+        difficulty = Mathf.Clamp01(difficulty);
+ 
+        int maxSpawnLength = (int)(foodSpawnPoints.Length * difficulty);
+
+        //Random food
+        int rf = UnityEngine.Random.Range(0, foodPrefabs.Length);
+        //Random spawnpoint
+        int rs = UnityEngine.Random.Range(0, maxSpawnLength);
+
+        Instantiate(foodPrefabs[rf], foodSpawnPoints[rs].transform.position, Quaternion.identity);
     }
 }
